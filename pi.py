fa9b5dd421774e8417366b2d16a64582bfe49463
Harrison <harry@simans.net>
2017-05-11 12:40:32 +0100

    Update pi.py
    Updated with network code and better parsing

--- pi.py
+++ pi.py
@@ -1,49 +1,54 @@
 import RPi.GPIO as GPIO
 import time
+import socket
+import sys
 def runsequence(name):
-	GPIO.setmode(GPIO.BOARD)
-	
-	for line in sequences[name]:
-		for pin in line[0]:
-			GPIO.setup(pin, GPIO.OUT)
-		for x in (GPIO.HIGH, GPIO.LOW):
-			for pin in line[0]:
-				GPIO.output(pin, x)
-			if x == GPIO.HIGH:
-				time.sleep(line[1] * 100)
-	GPIO.cleanup()
+    if not name in sequences:
+        print("Sequence not found")
+        return
+    GPIO.cleanup()
+    GPIO.setmode(GPIO.BOARD)
+    for line in sequences[name]:
+        for pin in line[0]:
+            GPIO.setup(pin, GPIO.OUT)
+        for x in (GPIO.HIGH, GPIO.LOW):
+            for pin in line[0]:
+                GPIO.output(pin, x)
+            if x == GPIO.HIGH:
+                time.sleep(line[1])
+    GPIO.cleanup()
+
 
 with open('config.txt') as f:
-    reading = False
     sequences = {}
+    lines = []
+    name = None
     l = [x.rstrip().lstrip() for x in f.readlines() if x != ""]
     for line in l:
-        if reading:
-            if line == "}":
-                if not lines:
-                    sys.exit("Empty block: " + name)
-                reading = False
-                sequences[name] = lines
-                lines = []
-            else:
-                if not pattern.match(line):
-                    sys.exit("Malformed entry: " + line)
-                split = line.split(":")
-                lines.append(([int(i) for i in split[0].split(",")], int(split[1])))
-            continue
- 
-        if not reading and line.endswith(" {"):
-            reading = True
+        if line.endswith(" {") and not name:
             name = line[:-2].rstrip()
-			if name == "":
-				sys.exit("Empty block name")
-			if len(name) > 25:
-				sys.exit(name + " longer than 25 chars")
+            if name == "":
+                sys.exit("Empty block name")
+            if len(name) > 25:
+                sys.exit(name + " longer than 25 chars")
+        elif name and pattern.match(line):
+            split = line.split(":")
+            lines.append(([int(i) for i in split[0].split(",")], int(split[1])))
+        elif line == "}" and lines:
+            sequences[name] = lines
+            name = None
+            lines = []
         else:
-            sys.exit("Cannot locate block for " + line)
-	if sequences.isempty():
-		sys.exit("Blank configuration file")
+            sys.exit("Parse error: " + l)
+    if sequences.isempty():
+        sys.exit("Blank configuration file")
 
-
-
-
+ip = "0.0.0.0"
+port = 1692
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((ip, port))
+while True:
+    data, addr = sock.recvfrom(30)
+    print("got request for " + data)
+    if len(data) < 30:
+        runsequence(str(data))
